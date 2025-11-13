e61d9b15a009f2ec8b48025a7945e4d8f29fa912
Alex <DA.Morrow@proton.me>
2025-11-13 16:38:00 +0100

    Glömt att pusha upp lite

--- utilities.cpp
+++ utilities.cpp
@@ -1,6 +1,12 @@
 #include "utilities.h"
 #include <ctime>
-
-std::time_t currentTimestamp() {
+#include <chrono>
+/*std::time_t currentTimestamp() {
     return std::time(nullptr);
+}*/
+
+std::time_t utils::now() {
+  auto now = std::chrono::system_clock::now();
+  auto result = std::chrono::system_clock::to_time_t(now);
+  return result;
 }
--- utilities.h
+++ utilities.h
@@ -3,6 +3,12 @@
 
 #include <ctime>
 
-std::time_t currentTimestamp();
+/*std::time_t currentTimestamp();*/
+std::string utils::timestamp_to_string(std::time_t& timestamp) {
+	std::tm tm = *std::gmtime(&timestamp); // UTC for "Z"
 
+	std::stringstream ss;
+	ss << std::put_time(&tm, "%F %T");
+	return ss.str();
+}
 #endif // UTILITIES_H
